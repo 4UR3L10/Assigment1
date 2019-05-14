d10987a5f403331061f271106fc8ed9a125765b2
4UR3L10 <aureliomartinezdros@outlook.com>
2019-05-13 21:37:39 -0400

    Last Update

---
+++ src/assignment1/Date.java
@@ -0,0 +1,27 @@
+package assignment1;
+
+public class Date {
+
+    private String month;
+    private String day;
+    private String year;
+
+    public Date(String month, String day, String year) {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+
+    public String getMonth() {
+        return month;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+}
---
+++ src/assignment1/Student.java
@@ -0,0 +1,50 @@
+package assignment1;
+
+import java.util.Date;
+
+public class Student {
+
+    private Name name;
+    private Address address;
+    //private Date date;
+    private String IDnumber;
+    private String Courses;
+    
+    /*
+    public Student(Name name, Address address, Date date, String IDnumber, String Courses) {
+        this.name = name;
+        this.address = address;
+        this.date = date;
+        this.IDnumber = IDnumber;
+        this.Courses = Courses;
+    }   
+    */
+
+    public Name getName() {
+        return name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    // public Date getDate() {  // Fix
+     //   return date;
+    //}
+
+    public String getIDnumber() {
+        return IDnumber;
+    }
+
+    public String getCourses() { //--------------
+        return Courses;
+    }   
+
+    public void addCourse() {
+        
+    }
+
+    public void dropCourse() { //---------------
+
+    }
+}
