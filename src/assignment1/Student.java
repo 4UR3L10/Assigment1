91a88978081781040ff293d200dab203dabdf68d
4UR3L10 <aureliomartinezdros@outlook.com>
2019-05-14 10:06:54 -0400

    Update Student.java
    Fixed Classes and doing set

--- src/assignment1/Student.java
+++ src/assignment1/Student.java
@@ -6,19 +6,17 @@ public class Student {
 
     private Name name;
     private Address address;
-    //private Date date;
+    private Date date;
     private String IDnumber;
     private String Courses;
-    
-    /*
+
     public Student(Name name, Address address, Date date, String IDnumber, String Courses) {
         this.name = name;
         this.address = address;
         this.date = date;
         this.IDnumber = IDnumber;
         this.Courses = Courses;
-    }   
-    */
+    }
 
     public Name getName() {
         return name;
@@ -28,20 +26,20 @@ public class Student {
         return address;
     }
 
-    // public Date getDate() {  // Fix
-     //   return date;
-    //}
+    public Date getDate() {
+        return date;
+    }
 
     public String getIDnumber() {
         return IDnumber;
     }
 
-    public String getCourses() { //--------------
+    public String getCourses() { //-------------
         return Courses;
-    }   
+    }
 
-    public void addCourse() {
-        
+    public void addCourse() {  //---------------
+
     }
 
     public void dropCourse() { //---------------
