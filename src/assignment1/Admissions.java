dfd5dd67a95298a24e110fd1e4b5f293a4d4eee7
Aurelio <aureliomartinezdros@outlook.com>
2019-05-16 18:58:50 -0400

    JPaneInput
    JPane
    Added functionality to Admission

--- src/assignment1/Admissions.java
+++ src/assignment1/Admissions.java
@@ -4,5 +4,20 @@ import java.util.ArrayList;
 
 public class Admissions
 {
+    private ArrayList<Student> list;
+    private Student stud;
+    private int index;
+    private boolean found;
 
+    public Admissions()
+    {
+        list = new ArrayList<Student>();
+    }
+    
+    public void add(Student std)
+    {
+        list.add(std);
+    }
+    
+    
 }
--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -1,5 +1,6 @@
 package assignment1;
 
+import java.text.DateFormat;
 import java.util.Date;
 
 public class Assignment1
@@ -7,6 +8,7 @@ public class Assignment1
 
     public static void main(String[] args)
     {
+        /*
         Name name1 = new Name("Aurelio", "Martinez");
         Address address1 = new Address("9281 SW 221 ST", "Miami", "FL", "33190");
         Date date1 = new Date();
@@ -25,6 +27,34 @@ public class Assignment1
         System.out.println("The information added is:\n" + "Name: " + stud1.getName().getFirstName() + " " + stud1.getName().getLastName() + "\n"
                 + "Address: " + stud1.getAddress().getStreet() + " " + stud1.getAddress().getCity() + " " + stud1.getAddress().getState() + " " + stud1.getAddress().getZip() + "\n"
                 + "Date: " + stud1.getDate().toString() + "\n" + "ID: " + stud1.getIDnumber() + "\n" + "Courses: " + stud1.getCourseArray());
+        */
+        
+        // Creating an Student.
+        // Creating a name object.
+        String firstName = getData.getWord("Enter First Name:");
+        String lastName = getData.getWord("Enter Last Name:");
+        Name name = new Name(firstName, lastName);
+        
+        // Creating an address object.
+        String street = getData.getWord("Enter your street:");
+        String city = getData.getWord("Enter your city:");
+        String state = getData.getWord("Enter your state:");
+        String zipCode = getData.getWord("Enter your zip code:");
+        Address address = new Address(street, city, state, zipCode);
+        
+        // Creating a date object.
+        DateFormat dateFormat = DateFormat.getDateInstance(DateFormat.LONG);
+        Date date = new Date();
+        
+        // Creating the courses.      Implementationnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn   ADDDDDDDD DROPPPPPPPPP
+        String courseOne = getData.getWord("Enter course one:");
+        String courseTwo = getData.getWord("Enter course two:");
+        String courseThird = getData.getWord("Enter course third:");
+        String courseFourth = getData.getWord("Enter course fourth:");
+        String courseFive = getData.getWord("Enter course five:");
+        String[] courseArray = {courseOne, courseTwo, courseThird, courseFourth, courseFive};
+        
+        
 
     }
 
