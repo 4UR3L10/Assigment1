1bd8b3e989b5eb1289d222084541b435168154f0
Aurelio <aureliomartinezdros@outlook.com>
2019-05-15 22:37:50 -0400

    Fixed Array
    Fix Array Problem
    Recent Stable version
    without comment
    -Student -Main

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -7,20 +7,20 @@ public class Assignment1 {
         Name name1 =  new Name("Aurelio", "Martinez");
         Address address1 = new Address("9281 SW 221 ST", "Miami", "FL", "33190");
         Date date1 = new Date();       
-        String[] courseArray = {"COP3337","ENC3245","SEN8976"};
+        String[] courseArray = {"COP3337","ENC3245","SEN8976","",""};
+        
+        
         
         Student stud1 = new Student(name1, address1, date1, "1001461532", courseArray);
         
+        stud1.addCourse("ASS4050");
+        stud1.addCourse("BOY4545");
         
         System.out.println("The information added is:\n" + "Name: " + stud1.getName().getFirstName() + " " + stud1.getName().getLastName() + "\n"
         + "Address: " + stud1.getAddress().getStreet() + " " + stud1.getAddress().getCity() + " " + stud1.getAddress().getState() + " " + stud1.getAddress().getZip() + "\n"
         + "Date: " + stud1.getDate().toString() + "\n" + "ID: " + stud1.getIDnumber() + "\n" + "Courses: " + stud1.getCourseArray());
         
-        
-//        System.out.println("Array:");
-//        for (int i = 0; i < courseArray.length; i++) {
-//            System.out.println(courseArray[i]);
-//        }
+            
 
 
     }
--- src/assignment1/Student.java
+++ src/assignment1/Student.java
@@ -3,23 +3,32 @@ package assignment1;
 import java.util.Date;
 
 
-public class Student {
 
+public class Student {
+    // Data Fields.
     private Name name;
     private Address address;
     private Date date;
     private String IDnumber;
-    //private String Courses;
-    private String[] courseArray = new String[5]; 
-
+    //private String Courses;????????????????????
+    private String[] courseArray = new String[5];
+    
+    
+    
+    
+    
+    // Constructor.
     public Student(Name name, Address address, Date date, String IDnumber, String Courses[]) {
         this.name = name;
         this.address = address;
         this.date = date;
         this.IDnumber = IDnumber;
+        // this.courseArray = Courses;
         this.courseArray = Courses;
+        
     }
-
+    
+    // Getters.
     public Name getName() {
         return name;
     }
@@ -35,12 +44,15 @@ public class Student {
     public String getIDnumber() {
         return IDnumber;
     }
-
+    
+    
+    // 
     public String getCourseArray() {
         // return courseArray;
         
         String arrayCounter = "";
         int arraySize = courseArray.length;
+        
         
         
         for (int i = 0; i < arraySize; i++) {
@@ -51,20 +63,38 @@ public class Student {
             else {
                 arrayCounter = arrayCounter + courseArray[i] + ",";
             }
-            // 0 1 2 
-            
-            
+            // 0 1 2                  
         }
         
         arrayCounter = arrayCounter + ".";  
         
+        //System.out.println(courseArray[4]);
+        
+        //for (int j = 0; j < 5; j++) {
+        //        System.out.println(courseArray[j]);
+        //}
         
         return arrayCounter;
     }
     
 
-    public void addCourse() {  //---------------
+    public void addCourse(String course) {  //---------------
+        // Get Size -> Check if there are no in use -> add it -> 
+        //-> esle no espace
+        int arraySize = courseArray.length;
         
+//        if (arraySize == 5) {
+//            System.out.println("Sorry the student has already the maximum of courses");
+//        } else {
+            for (int i = 0; i < 5; i++) {
+                if (courseArray[i] == "") {
+                    courseArray[i] = course; 
+                    
+                    i = 5;
+                }
+                
+            }
+//        }
     }
 
     public void dropCourse() { //---------------
