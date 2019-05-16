43d3c8ce90e0041eeee5357cdd1d200f89f49829
Aurelio <aureliomartinezdros@outlook.com>
2019-05-16 14:46:42 -0400

    Update Assignment1.java
    Fixed main class to right format

--- src/assignment1/Assignment1.java
+++ src/assignment1/Assignment1.java
@@ -1,30 +1,31 @@
 package assignment1;
+
 import java.util.Date;
 
-public class Assignment1 {
-  
-    public static void main(String[] args) {
-        Name name1 =  new Name("Aurelio", "Martinez");
+public class Assignment1
+{
+
+    public static void main(String[] args)
+    {
+        Name name1 = new Name("Aurelio", "Martinez");
         Address address1 = new Address("9281 SW 221 ST", "Miami", "FL", "33190");
-        Date date1 = new Date();       
-        String[] courseArray = {"COP3337","ENC3245","SEN8976","",""};
-        
-        
-        
+        Date date1 = new Date();
+        String[] courseArray =
+        {
+            "COP3337", "ENC3245", "SEN8976", "", ""
+        };
+
         Student stud1 = new Student(name1, address1, date1, "1001461532", courseArray);
-        
+
         stud1.addCourse("ASS4050");
         stud1.addCourse("BOY4545");
         //stud1.dropCourse("ASS4050");
         // stud1.addCourse("CUL9090");
-        
-        System.out.println("The information added is:\n" + "Name: " + stud1.getName().getFirstName() + " " + stud1.getName().getLastName() + "\n"
-        + "Address: " + stud1.getAddress().getStreet() + " " + stud1.getAddress().getCity() + " " + stud1.getAddress().getState() + " " + stud1.getAddress().getZip() + "\n"
-        + "Date: " + stud1.getDate().toString() + "\n" + "ID: " + stud1.getIDnumber() + "\n" + "Courses: " + stud1.getCourseArray());
-        
-            
 
+        System.out.println("The information added is:\n" + "Name: " + stud1.getName().getFirstName() + " " + stud1.getName().getLastName() + "\n"
+                + "Address: " + stud1.getAddress().getStreet() + " " + stud1.getAddress().getCity() + " " + stud1.getAddress().getState() + " " + stud1.getAddress().getZip() + "\n"
+                + "Date: " + stud1.getDate().toString() + "\n" + "ID: " + stud1.getIDnumber() + "\n" + "Courses: " + stud1.getCourseArray());
 
     }
-    
+
 }
